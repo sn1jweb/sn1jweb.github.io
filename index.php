8abad53532e486f00a2b36273c8f817e0121a547
sn1jweb <72201924+sn1jweb@users.noreply.github.com>
2020-10-01 15:46:57 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php $title = "EthFair"; include_once 'blocks/head.php'?>
+</head>
+<body>
+  <?php include_once "blocks/header.php"?>
+  <div id="wrapper">
+    <div id="bigArticle">
+        <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="100">
+      <h1>EthFair</h2>
+        </div>
+        <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="500">
+      <h2>Don't wait for a pump <br><br>Get a lambo now!!!</h2>
+        </div>
+        <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="1000">
+      <img src="img/lambo.png">
+        </div>
+    </div>
+  </div>
+  <?php include_once "blocks/footer.php"?>
+</body>
+</html>
---
+++ prizes.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php $title = "Prizes | EthFair"; include_once 'blocks/head.php'?>
+</head>
+<body>
+  <?php include_once "blocks/header.php"?>
+  <div id="prizes">
+    <div id="prizesArticle">
+        <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="100">
+      <img src="img/prize-lol.png">
+        </div>
+    </div>
+  </div>
+  <?php include_once "blocks/footer.php"?>
+</body>
+</html>
